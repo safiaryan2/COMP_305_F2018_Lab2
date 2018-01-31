0ad99f28e9437372d1d6b3dea049abfd8777411e
Safi Ullah Tanvir <stanvir2@my.centennialcollege.ca>
2018-01-31 18:23:24 -0500

    Initial commit

---
+++ Assets/Scripts/ClickHammer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHammer : MonoBehaviour {
+
+
+	public GameObject Player;
+	public Sprite firstImage;
+	public Sprite secondImage;
+
+	private SpriteRenderer sRend;
+	private Rigidbody2D rBody;
+
+	// initiaisation
+
+	void Start()
+	{
+		// Player.velocity = new Vector2 (5, 0);
+		rBody = Player.GetComponent<Rigidbody2D> ();
+		sRend = Player.GetComponent<SpriteRenderer> ();
+		 rBody.velocity = new Vector2 (3, 0);
+
+
+	}
+
+	void Update()
+	{
+
+
+	}
+
+	void OnMouseDown () {
+		// GameObject gObject = GameObject.Find ("Player");
+		if (sRend.sprite = firstImage) {
+			
+			sRend.sprite = secondImage;
+		}
+		else 
+		{
+			sRend.sprite = firstImage;
+		}
+
+
+
+
+		// Debug.Log (gObject.name);
+	}
+}
